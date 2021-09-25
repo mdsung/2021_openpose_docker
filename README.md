dcea8bb13b6a2f428db02d97f74d71377c2ed5c5
mdskylover@gmail.com <mindong@localhost.localdomain>
2021-09-25 22:39:20 +0900

    can manage space in path & test passed docker-compose up

--- README.md
+++ README.md
@@ -4,11 +4,13 @@
 ## Date
     2021.09.25
 ## Objective
-- run openpose 
+- Run openpose on targeted folder 
+- Ignore already processed videos.
 ## Prerequisite
 - cuda 10.1 or higher
 - docker
 - [nvidia-docker](https://github.com/NVIDIA/nvidia-docker)
+- [docker-compose](https://docs.docker.com/compose/)
 
 ## Project structure
 * `code/build_docker.sh`: script for build docker image
@@ -16,16 +18,10 @@
 * `code/process.py`: python script to list target movies and to run openpose to target movies.
 * `code/postprocess.py`: python script to change chown and add "_out" if the name doesn't contain '_out' after openpose was applied
 * `dockerfile`: dockerfile for running openpose in any environment
+* `docker-compose.yml`: docker-compose file for all process
 
 ## Process
-1. build dockerfile
-    ```
-    code/build_docker.sh
-    ```
-2. run docker image
-    ```
-    code/run_docker.sh
-    ```
+1. Run `docker-compose up`
 
 ## Reference
 * [https://github.com/CMU-Perceptual-Computing-Lab/openpose](https://github.com/CMU-Perceptual-Computing-Lab/openpose)
--- code/process.py
+++ code/process.py
@@ -9,16 +9,13 @@ PROCESSED_DATA_PATH = Path('/data/processed/core/assembly_skeletal/')
 # RAW_DATA_PATH = Path('/home/nia_data/processed/core/assembly/')
 # PROCESSED_DATA_PATH = Path('/home/nia_data/processed/core/assembly_skeletal/')
 
-PROGRAM_PATH = Path('/openpose/build/examples/openpose/openpose.bin')
-
-def check_models_folder():
-    return Path(CURRENT_PATH, "model").exists('models')
+EXCEPT_PATH = ['B001', 'D002']
 
 def run_openpose(input_path, output_path):
     """
     /openpose/build/examples/openpose/openpose.bin --video /data/processed/core/assembly/B031/B031_GMS_1_1.mp4 --display 0 --hand --write_video /data/processed/core/assembly_skeletal/B031/B031_GMS_1_1_out.mp4
     """
-    cmd = f"/openpose/build/examples/openpose/openpose.bin --video {input_path} --display 0 --hand  --write_video {output_path}"
+    cmd = f"/openpose/build/examples/openpose/openpose.bin --video '{input_path}' --display 0 --hand  --write_video '{output_path}'"
     subprocess.run(cmd, shell = True)
     
 def read_folder_mp4_contents(path:Path):
@@ -28,8 +25,10 @@ def create_output_folder(output_folder):
     output_folder.mkdir(parents=True, exist_ok=True)
     
 def get_output_folder(input_file:Path):
-    output_file = Path(str(input_file).replace('assembly', 'assembly_skeletal'))
-    output_file = Path(str(input_file).replace('.mp4', '_out.mp4'))
+    output_file_name = str(input_file).\
+                            replace('assembly', 'assembly_skeletal').\
+                            replace('.mp4', '_out.mp4')
+    output_file = Path(output_file_name)
     create_output_folder(output_file.parent)
     return output_file
 
@@ -40,6 +39,10 @@ def main():
     target_mp4_list = [f for f in raw_mp4_list if f.name not in processed_mp4_name_list]
 
     for mp4_file in target_mp4_list:
+        if "B001" in str(mp4_file):
+            continue
+        if "D002" in str(mp4_file):
+            continue
         input_path = mp4_file
         output_path = get_output_folder(mp4_file)
         run_openpose(input_path, output_path)
