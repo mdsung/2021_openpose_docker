3fb56df4dd95e0493057c09b7b7bd31ce1671ad8
mdskylover@gmail.com <mindong@localhost.localdomain>
2021-09-27 11:24:56 +0900

    Add get_progress.py

--- code/process.py
+++ code/process.py
@@ -9,11 +9,16 @@ PROCESSED_DATA_PATH = Path('/data/processed/core/assembly_json/')
 # RAW_DATA_PATH = Path('/home/nia_data/processed/core/assembly/')
 # PROCESSED_DATA_PATH = Path('/home/nia_data/processed/core/assembly_json')
 
-def run_openpose(input_path, output_path, json_output_path):
+def run_openpose(input_path, output_path, json_output_path, rendering=False):
     """
-    /openpose/build/examples/openpose/openpose.bin --video /data/processed/core/assembly/B031/B031_GMS_1_1.mp4 --display 0 --hand --write_video /data/processed/core/assembly_skeletal/B031/B031_GMS_1_1_out.mp4 --write_json /data/processed/core/assembly_json/B031 --disable_blending
+    command exmaple:
+        /openpose/build/examples/openpose/openpose.bin --video /data/processed/core/assembly/B031/B031_GMS_1_1.mp4 --display 0 --hand --write_video /data/processed/core/assembly_skeletal/B031/B031_GMS_1_1_out.mp4 --write_json /data/processed/core/assembly_json/B031 --disable_blending
     """
-    cmd = f"/openpose/build/examples/openpose/openpose.bin --video '{input_path}' --display 0 --hand --write_json '{json_output_path}' --render_pose 0 --disable_blending"
+    if rendering:
+        cmd = f"/openpose/build/examples/openpose/openpose.bin --video '{input_path}' --display 0 --hand --write_json '{json_output_path}' --write_video '{output_path}' --disable_blending"
+    else:
+        cmd = f"/openpose/build/examples/openpose/openpose.bin --video '{input_path}' --display 0 --hand --write_json '{json_output_path}' --render_pose 0 --disable_blending"
+        
     subprocess.run(cmd, shell = True)
     
 def read_folder_mp4_contents(path:Path, extension:str):
@@ -50,7 +55,6 @@ def main():
     processed_json_name_list = get_json_file_name_list(processed_json_list)
     target_mp4_list = get_target_file_list(raw_mp4_list, processed_json_name_list)
 
-    print(target_mp4_list)
     for mp4_file in target_mp4_list:
         input_path = mp4_file
         output_path = get_output_folder(mp4_file)
