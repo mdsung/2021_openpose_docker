c3b59e215107b40ecca3efc2d4a5aad1c76d7855
mdskylover@gmail.com <mindong@localhost.localdomain>
2021-09-27 10:13:28 +0900

    change processed check from mp4 to json file & processed only json without rendering

--- code/process.py
+++ code/process.py
@@ -5,22 +5,30 @@ from pathlib import Path
 
 CURRENT_PATH = Path(__file__)
 RAW_DATA_PATH = Path('/data/processed/core/assembly/')
-PROCESSED_DATA_PATH = Path('/data/processed/core/assembly_skeletal/')
+PROCESSED_DATA_PATH = Path('/data/processed/core/assembly_json/')
 # RAW_DATA_PATH = Path('/home/nia_data/processed/core/assembly/')
-# PROCESSED_DATA_PATH = Path('/home/nia_data/processed/core/assembly_skeletal/')
+# PROCESSED_DATA_PATH = Path('/home/nia_data/processed/core/assembly_json')
 
 def run_openpose(input_path, output_path, json_output_path):
     """
-    /openpose/build/examples/openpose/openpose.bin --video /data/processed/core/assembly/B031/B031_GMS_1_1.mp4 --display 0 --hand --write_video /data/processed/core/assembly_skeletal/B031/B031_GMS_1_1_out.mp4
+    /openpose/build/examples/openpose/openpose.bin --video /data/processed/core/assembly/B031/B031_GMS_1_1.mp4 --display 0 --hand --write_video /data/processed/core/assembly_skeletal/B031/B031_GMS_1_1_out.mp4 --write_json /data/processed/core/assembly_json/B031 --disable_blending
     """
-    cmd = f"/openpose/build/examples/openpose/openpose.bin --video '{input_path}' --display 0 --hand  --write_video '{output_path}' --write_json {json_output_path} --disable_blending"
+    cmd = f"/openpose/build/examples/openpose/openpose.bin --video '{input_path}' --display 0 --hand --write_json '{json_output_path}' --render_pose 0 --disable_blending"
     subprocess.run(cmd, shell = True)
     
-def read_folder_mp4_contents(path:Path):
-    return path.glob('**/*.mp4')
+def read_folder_mp4_contents(path:Path, extension:str):
+    return path.glob(f'**/*.{extension}')
 
 def create_output_folder(output_folder):
     output_folder.mkdir(parents=True, exist_ok=True)
+
+def get_json_file_name_list(json_list):
+    return list({"_".join(js.name.split("_")[:4]) for js in json_list})
+
+def get_target_file_list(raw_mp4_list, processed_json_name_list):
+    return [f for f in raw_mp4_list \
+            if (f.name not in processed_json_name_list) & \
+                (f.parent.name.startswith('D'))]
 
 def get_json_output_folder(input_file:Path):
     json_output_file_name = str(input_file.parents[0]).\
@@ -37,16 +45,13 @@ def get_output_folder(input_file:Path):
     return output_file
 
 def main():
-    raw_mp4_list = read_folder_mp4_contents(RAW_DATA_PATH)
-    processed_mp4_list = read_folder_mp4_contents(PROCESSED_DATA_PATH)
-    processed_mp4_name_list = [mp4_file.name.replace('_out','') for mp4_file in processed_mp4_list]
-    target_mp4_list = [f for f in raw_mp4_list if (f.name not in processed_mp4_name_list) & (f.parent.name.startswith('D'))]
+    raw_mp4_list = read_folder_mp4_contents(RAW_DATA_PATH, "mp4")
+    processed_json_list = read_folder_mp4_contents(PROCESSED_DATA_PATH, 'json')
+    processed_json_name_list = get_json_file_name_list(processed_json_list)
+    target_mp4_list = get_target_file_list(raw_mp4_list, processed_json_name_list)
 
+    print(target_mp4_list)
     for mp4_file in target_mp4_list:
-        if "B001" in str(mp4_file):
-            continue
-        if "D002" in str(mp4_file):
-            continue
         input_path = mp4_file
         output_path = get_output_folder(mp4_file)
         json_output_path = get_json_output_folder(mp4_file)
